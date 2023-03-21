2f80a82ca7014db2f0a1c12766326a1940fd50a2
flaviosoliver <flavsoliver@gmail.com>
2023-03-21 01:09:36 -0300

    upload projeto

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import MovieLibrary from './components/MovieLibrary';
+import Header from './components/Header';
+import movies from './data';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MovieLibrary movies={movies} />
+
+    </div>
+  );
+}
+
+export default App;
