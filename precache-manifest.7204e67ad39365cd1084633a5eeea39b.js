ed33b4a2bea71f8bc4a45e7a435ea4fc92b4e915
flaviosoliver <flavsoliver@gmail.com>
2023-03-21 01:14:28 -0300

    Updates

---
+++ precache-manifest.7204e67ad39365cd1084633a5eeea39b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba35a27a25d010fb58429d79a533976",
+    "url": "/project-movie-card-library-stateful/index.html"
+  },
+  {
+    "revision": "5db10fb83179f7cf72b2",
+    "url": "/project-movie-card-library-stateful/static/css/main.5d58c8a2.chunk.css"
+  },
+  {
+    "revision": "d55231959b3091c9b9b8",
+    "url": "/project-movie-card-library-stateful/static/js/2.0efb54d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-movie-card-library-stateful/static/js/2.0efb54d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5db10fb83179f7cf72b2",
+    "url": "/project-movie-card-library-stateful/static/js/main.4d1b8e26.chunk.js"
+  },
+  {
+    "revision": "fe8a66e0822c76510478",
+    "url": "/project-movie-card-library-stateful/static/js/runtime-main.e2fe6cce.js"
+  }
+]);
